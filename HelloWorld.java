a653e5556c2d69c73fb36b52cc518b5c4d6de002
Lavanya072002 <160476264+Lavanya072002@users.noreply.github.com>
2024-02-22 16:37:43 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!...\n In this java is being run with the help of dockerfile");
     }
 }
